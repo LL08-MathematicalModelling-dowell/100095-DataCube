da7a94989cba25bca160c07d3b0de00990444f97
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-09-23 19:11:56 +0100

    Removed unnecessary Apps

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from .views import (
     DataCrudView, GetDataView,
-    CollectionView, CreateDatabaseView,
-    ListCollectionsView, AddCollectionView,
-    )
+    CreateDatabaseView, ListCollectionsView,
+    AddCollectionView,
+)
 
 app_name = 'db_api'
 
 urlpatterns = [
     path('crud/', DataCrudView.as_view(), name="crud"),
     path('get_data/', GetDataView.as_view(), name="get_data"),
-    path('collections/', CollectionView.as_view(), name='collections'),
+    # path('collections/', CollectionView.as_view(), name='collections'),
     path('create_database/', CreateDatabaseView.as_view(), name='create_database'),
     path('list_collections/', ListCollectionsView.as_view(), name='list_collections'),
-    path('add_collection/', CreateCollectionView.as_view(), name='add_collection'),
+    path('add_collection/', AddCollectionView.as_view(), name='add_collection'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -15,16 +15,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api/', include('dbdetails.urls')),
-    path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('db_api/', include('api.urls')),
-
+    path('api/', include('api.urls')),
     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('api/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
 ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
