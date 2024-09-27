6753f79fc57c93fedb50225006a555fb4587de0a
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-09-27 15:35:12 +0100

    Improved code quality and structure

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,6 @@ app_name = 'api'
 urlpatterns = [
     path('crud/', DataCrudView.as_view(), name="crud"),
     path('get_data/', GetDataView.as_view(), name="get_data"),
-    # path('collections/', CollectionView.as_view(), name='collections'),
     path('create_database/', CreateDatabaseView.as_view(), name='create_database'),
     path('list_collections/', ListCollectionsView.as_view(), name='list_collections'),
     path('add_collection/', AddCollectionView.as_view(), name='add_collection'),
